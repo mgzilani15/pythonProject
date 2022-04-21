fb5f3d0cf6ce64d07ed62730a02c874d18c09240
mgzilani15 <golam_zilani2001@yahoo.com>
2022-04-22 03:11:36 +1000

    Initial commit

---
+++ my 2 python file.py
@@ -0,0 +1,24 @@
+#this variable is used for adding two numbers
+addition = 2 + 2
+print(addition)
+
+import keyword
+
+print(keyword.kwlist)
+
+sentence1 = "This is a string in double quotes"
+
+sentence2 = """This is a multiline string 
+   Line 1 added 
+   Line 2 added 
+   Line 3 added 
+   """
+
+sentence3 = "This is a longer string"
+
+
+def say_hi():
+    print('Hello and welcome to my program')
+
+
+say_hi()
